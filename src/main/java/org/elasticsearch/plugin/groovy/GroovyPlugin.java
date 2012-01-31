42dade8a91c9aba5792f5335397a328c7b4ab5d4
Shay Banon <kimchy@gmail.com>
2012-01-31 13:09:40 +0200

    move to elasticsearch 0.19.0 snap and use some of its features

--- src/main/java/org/elasticsearch/plugin/groovy/GroovyPlugin.java
+++ src/main/java/org/elasticsearch/plugin/groovy/GroovyPlugin.java
@@ -19,7 +19,6 @@
 
 package org.elasticsearch.plugin.groovy;
 
-import org.elasticsearch.common.inject.Module;
 import org.elasticsearch.plugins.AbstractPlugin;
 import org.elasticsearch.script.ScriptModule;
 import org.elasticsearch.script.groovy.GroovyScriptEngineService;
@@ -39,10 +38,7 @@ public class GroovyPlugin extends AbstractPlugin {
         return "Groovy plugin allowing to add groovy scripting support";
     }
 
-    @Override
-    public void processModule(Module module) {
-        if (module instanceof ScriptModule) {
-            ((ScriptModule) module).addScriptEngine(GroovyScriptEngineService.class);
-        }
+    public void onModule(ScriptModule module) {
+        module.addScriptEngine(GroovyScriptEngineService.class);
     }
 }

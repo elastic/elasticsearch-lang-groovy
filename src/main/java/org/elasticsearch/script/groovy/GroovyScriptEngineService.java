e364fd20432485282d5b79693dade61d2c38de53
David Pilato <david@pilato.fr>
2013-02-23 10:22:28 +0100

    Move to Elasticsearch 0.21.0.Beta1 Due to refactoring in 0.21.x we have to update this plugin Closes #9.

--- src/main/java/org/elasticsearch/script/groovy/GroovyScriptEngineService.java
+++ src/main/java/org/elasticsearch/script/groovy/GroovyScriptEngineService.java
@@ -22,7 +22,7 @@ package org.elasticsearch.script.groovy;
 import groovy.lang.Binding;
 import groovy.lang.GroovyClassLoader;
 import groovy.lang.Script;
-import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.AtomicReaderContext;
 import org.apache.lucene.search.Scorer;
 import org.elasticsearch.common.Nullable;
 import org.elasticsearch.common.component.AbstractComponent;
@@ -170,8 +170,8 @@ public class GroovyScriptEngineService extends AbstractComponent implements Scri
         }
 
         @Override
-        public void setNextReader(IndexReader reader) {
-            lookup.setNextReader(reader);
+        public void setNextReader(AtomicReaderContext context) {
+            lookup.setNextReader(context);
         }
 
         @Override

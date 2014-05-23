95827a6c083b70e5ce0f9d8a58ea6dcaadb1392d
David Pilato <david@pilato.fr>
2014-05-23 20:12:45 +0200

    Update to elasticsearch 1.3.0
    Closes #28

--- src/test/java/org/elasticsearch/script/groovy/GroovyScriptSearchTests.java
+++ src/test/java/org/elasticsearch/script/groovy/GroovyScriptSearchTests.java
@@ -21,7 +21,10 @@ package org.elasticsearch.script.groovy;
 
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders;
+import org.elasticsearch.plugins.PluginsService;
 import org.elasticsearch.search.sort.SortOrder;
 import org.elasticsearch.test.ElasticsearchIntegrationTest;
 import org.hamcrest.CoreMatchers;
@@ -42,7 +45,16 @@ import static org.hamcrest.Matchers.equalTo;
 /**
  *
  */
+@ElasticsearchIntegrationTest.ClusterScope(scope = ElasticsearchIntegrationTest.Scope.SUITE)
 public class GroovyScriptSearchTests extends ElasticsearchIntegrationTest {
+
+    @Override
+    protected Settings nodeSettings(int nodeOrdinal) {
+        return ImmutableSettings.builder()
+                .put(super.nodeSettings(nodeOrdinal))
+                .put("plugins." + PluginsService.LOAD_PLUGIN_FROM_CLASSPATH, true)
+                .build();
+    }
 
     @Test
     public void testGroovyScriptFilter() throws Exception {
